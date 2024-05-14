e62c5e1d630e5d14084da7785cf12da1b2c33883
VMCalhau <vitmelocalhau@gmail.com>
2024-05-14 16:27:04 -0300

    feat: inicia desenvolvimento da página de listagem de moedas; fix: atualiza navbar para colapsar ao selecionar página; fix: define tamanho máximo para informações mostradas na página de conversão

--- src/app/components/pages/lista-moedas/lista-moedas.component.ts
+++ src/app/components/pages/lista-moedas/lista-moedas.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CurrencyService } from '../../../services/currency.service';
+import { Moeda } from '../../../interfaces/moeda';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-lista-moedas',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './lista-moedas.component.html',
   styleUrl: './lista-moedas.component.css'
 })
-export class ListaMoedasComponent {
+export class ListaMoedasComponent implements OnInit {
+  listaMoedas!: Moeda[];
+  colunasTabela: string[] = ['codigo', 'nome'];
 
+  constructor (private currencyService: CurrencyService) {}
+
+  ngOnInit(): void {
+    this.currencyService.getCurrencies().subscribe((moedas) => (this.listaMoedas = moedas));
+  }
 }
