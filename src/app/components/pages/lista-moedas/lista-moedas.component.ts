5da17af176d53a92fe4bdd5ebfa88e2477d762de
VMCalhau <vitmelocalhau@gmail.com>
2024-05-15 11:07:22 -0300

    feat: termina desenvolvimento de página de listagem de moedas; fix: conserta footer para ficar no fim da página, mesmo que ela cresça dinamicamente

--- src/app/components/pages/lista-moedas/lista-moedas.component.ts
+++ src/app/components/pages/lista-moedas/lista-moedas.component.ts
@@ -1,22 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { CurrencyService } from '../../../services/currency.service';
 import { Moeda } from '../../../interfaces/moeda';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, Sort, MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-moedas',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule, MatFormFieldModule, FormsModule, MatInputModule],
   templateUrl: './lista-moedas.component.html',
   styleUrl: './lista-moedas.component.css'
 })
-export class ListaMoedasComponent implements OnInit {
-  listaMoedas!: Moeda[];
+export class ListaMoedasComponent implements OnInit, AfterViewInit {
+  dataSource = new MatTableDataSource<Moeda>([]);
   colunasTabela: string[] = ['codigo', 'nome'];
+  valorFiltro!: string;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor (private currencyService: CurrencyService) {}
 
   ngOnInit(): void {
-    this.currencyService.getCurrencies().subscribe((moedas) => (this.listaMoedas = moedas));
+    this.currencyService.getCurrencies().subscribe((moedas) => (this.dataSource.data = moedas));
+  }
+  
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  onChangeFilter(): void {
+    this.dataSource.filter = this.valorFiltro;
   }
 }
